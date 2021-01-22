7128bde7550185a4b6737cb041c9abdbdb8c56fe
Luis Fernando V. Leal <luisfernandovl.dev@gmail.com>
2021-01-21 23:30:53 -0300

    Site

---
+++ src/script.js
@@ -0,0 +1,22 @@
+window.onscroll = function() {myFunction()};
+
+var barratopo = document.getElementsByClassName("barratopo")[0];
+var sticky = barratopo.offsetTop;
+
+var irtopo = document.getElementsByClassName("irtopo")[0];
+
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    barratopo.style.paddingTop=0;
+    barratopo.style.display='block';
+    barratopo.classList.add("sticky");
+
+    irtopo.style.display='block'
+  } else {
+    barratopo.style.display='none';
+    barratopo.classList.remove("sticky");
+    
+    irtopo.style.display='none'
+  }
+}
